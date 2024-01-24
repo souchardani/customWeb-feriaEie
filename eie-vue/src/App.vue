288e0f5b379176887ad089e3088cb9bf11ea0a2f
souchardani <114416167+souchardani@users.noreply.github.com>
2024-01-24 12:01:36 +0100

    cambios a vue

---
+++ eie-vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppLayout from "./components/layouts/AppLayout.vue";
+import HeroSection from "./components/HeroSection.vue";
+</script>
+
+<template>
+  <AppLayout>
+    <main class="space-y-40 mb-40">
+      <HeroSection />
+      <Features />
+      <Stats />
+      <Testimonials />
+      <CallToAction />
+      <Blog />
+    </main>
+  </AppLayout>
+</template>
---
+++ eie-vue/src/components/layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <AppHeader />
+  <slot></slot>
+  <AppFooter />
+</template>
+
+<style scoped>
+html {
+  font-family: Urbanist, sans-serif;
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: 0;
+}
+</style>
+
+<script setup>
+import AppHeader from "../AppHeader.vue";
+import AppFooter from "../AppFooter.vue";
+</script>
